eb006aeb1ddc92d8f946251420670d43fd7cc1fc
Navjot Singh <navjotsingh9633@gmail.com>
2017-10-16 12:39:23 +0530

    init directories

---
+++ app/src/main/java/com/navjot/deepak/manpreet/pdfsociety/controller/delete.java
@@ -0,0 +1,8 @@
+package com.navjot.deepak.manpreet.pdfsociety.controller;
+
+/**
+ * Created by hp on 10/16/2017.
+ */
+
+public class delete {
+}
---
+++ app/src/main/java/com/navjot/deepak/manpreet/pdfsociety/model/delete.java
@@ -0,0 +1,8 @@
+package com.navjot.deepak.manpreet.pdfsociety.model;
+
+/**
+ * Created by hp on 10/16/2017.
+ */
+
+public class delete {
+}
