82fa7e8e8e2f42408bc04f5b14872d878e8559da
Navjot Singh <navjotsingh9633@gmail.com>
2017-10-20 11:27:49 +0530

    splash activity created

---
+++ app/src/main/java/com/navjot/deepak/manpreet/pdfsociety/Activities/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.navjot.deepak.manpreet.pdfsociety.Activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.Transaction;
+import com.navjot.deepak.manpreet.pdfsociety.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler.sendEmptyMessageDelayed(101,2000);
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 101){
+                startActivity(new Intent(SplashActivity.this, SignIn.class));
+                finish();
+            }
+        }
+    };
+}
