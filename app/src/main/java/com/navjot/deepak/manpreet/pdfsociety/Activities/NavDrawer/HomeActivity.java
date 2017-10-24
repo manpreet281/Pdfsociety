f496e5f0da4bd55c0d1d2769efaa4b01208c5c9c
dpruthi <dpruthi16@gmail.com>
2017-10-24 11:41:08 +0530

    log out and share apk

--- app/src/main/java/com/navjot/deepak/manpreet/pdfsociety/Activities/NavDrawer/HomeActivity.java
+++ app/src/main/java/com/navjot/deepak/manpreet/pdfsociety/Activities/NavDrawer/HomeActivity.java
@@ -1,6 +1,12 @@
 package com.navjot.deepak.manpreet.pdfsociety.Activities.NavDrawer;
 
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.net.Uri;
 import android.os.Bundle;
+
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -13,10 +19,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.navjot.deepak.manpreet.pdfsociety.Activities.SignIn;
 import com.navjot.deepak.manpreet.pdfsociety.R;
 
+import java.io.File;
+
 public class HomeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+
+        implements NavigationView.OnNavigationItemSelectedListener
+
+{
+    public ProgressDialog mProgressDialog;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +74,11 @@ public class HomeActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
+        // Inflate menu resource file.
+
+
         return true;
+
     }
 
     @Override
@@ -72,6 +92,15 @@ public class HomeActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
+        if (id == R.id.action_logout) {
+
+
+                    showProgressDialog();
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(HomeActivity.this, SignIn.class));
+                    return true;
+                }
+
 
         return super.onOptionsItemSelected(item);
     }
@@ -91,6 +120,19 @@ public class HomeActivity extends AppCompatActivity
         } else if (id == R.id.nav_manage) {
 
         } else if (id == R.id.nav_share) {
+            item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem menuItem) {
+                    ApplicationInfo applicationInfo = getApplicationContext().getApplicationInfo();
+                    String apkPath = applicationInfo.sourceDir;
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.setType("application/vnd.android.package-archieve");
+                    intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(apkPath)));
+                    startActivity(Intent.createChooser(intent, "Share App Using"));
+
+                    return false;
+                }
+            });
 
         } else if (id == R.id.nav_send) {
 
@@ -100,4 +142,17 @@ public class HomeActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage(getString(R.string.loading));
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
 }
+
+
+
