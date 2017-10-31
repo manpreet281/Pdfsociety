b0729f179274da5dd02adeeaae91906851b867cf
Navjot Singh <navjotsingh9633@gmail.com>
2017-10-31 17:56:25 +0530

    created recent fragment

---
+++ app/src/main/java/com/navjot/deepak/manpreet/pdfsociety/Fragments/MyPdfsFragment.java
@@ -0,0 +1,31 @@
+package com.navjot.deepak.manpreet.pdfsociety.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.navjot.deepak.manpreet.pdfsociety.R;
+
+public class MyPdfsFragment extends PdfListFragment {
+
+    public MyPdfsFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START my_top_pdfs_query]
+        // My top pdfs by number of stars
+        String myUserId = getUid();
+        Query myToppdfsQuery = databaseReference.child("user-pdfs").child(myUserId)
+                .orderByChild("starCount");
+        // [END my_top_pdfs_query]
+
+        return myToppdfsQuery;
+    }
+
+
+}
